95812b20d29a113b4192c520a9b02f23cd8ae1f9
her0e1c1 <her0e1c1@gmail.com>
2015-08-24 02:41:58 +0900

    add game/admin.py

---
+++ napoleon/game/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.models import User
+from . import models
+# from . import forms
+
+# Register your models here.
+
+
+# class UsersInline(admin.StackedInline):
+class UsersInline(admin.TabularInline):
+    model = models.Game.players.through
+
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    fields = ["label"]
+    # list_display = ["label"]
+    # list_display_links = ["label"]
+    inlines = [UsersInline]
