02ed0ba22e93058603efb5d579c2f1ad38d43621
her0e1c1 <her0e1c1@gmail.com>
2015-08-25 02:20:32 +0900

    js updated

--- napoleon/static/js/game.js
+++ napoleon/static/js/game.js
@@ -9,7 +9,6 @@ app.controller("GameController", ["$scope", function($scope){
     var wsGame;
     var self = this;
     var sid = $.cookie("sessionid");
-
     self.sid = sid;
     self.unused = [];
 
@@ -19,52 +18,84 @@ app.controller("GameController", ["$scope", function($scope){
 
             self.is_my_turn = self.user_id == data.turn;
             self.is_napoleon = self.user_id == data.napoleon;
-            self.is_passed = self.pass_ids.indexOf(self.user_id) >= 0;
+            self.is_passed = elem(self.pass_ids, self.user_id);
 
             $scope.$apply();
+
+            if (data.waiting_next_turn){
+                var time = 1 * 1000;
+                $("body").delay(time).queue(function(){
+                    self.next_turn();
+                });
+            }
         });
     };
 
     $scope.$watch("game.game_id", function(){
         self.update();
-        wsGame = new WebSocket("ws://192.168.56.2:8888/game/" + self.game_id); 
+        wsGame = new WebSocket("ws://192.168.56.2:8888/game/" + self.game_id);
         wsGame.onmessage = function (evt) {
-            var json = JSON.parse(evt.data); 
+            var json = JSON.parse(evt.data);
             if (json.update)
                 self.update();  // 本来はAjaxでなくWebSocketで更新すべき
         };
     });
 
     this.send = function (json){
+        json["session_id"] = sid;
         wsGame.send(JSON.stringify(json));
     };
-    
+
+    self.next_turn = function(){
+        self.send({"next_turn": true});
+    };
+
     this.pass = function(){
-        self.send({"mode": "pass", "session_id": sid});
+        self.send({"mode": "pass"});
     };
 
     self.is_unused_careds_selected = function(){
         return self.unused.length >= self.rest.length;
     };
 
-    self.select = function(card){
-        if (!self.is_my_turn)
-            return;
+    self.select_or_discard = function(card){
+        if (self.phase == 'discard' && self.is_napoleon)
+            self.discard(card);
+        else if (self.is_my_turn && (self.phase == 'first_round' || self.phase == 'rounds'))
+            self.select(card);
+    };
 
+    self.discard = function(card){
+        var unused = self.unused.map(function(i){ return i.value;});
         if (self.is_unused_careds_selected())
-            self.send({"mode": "unused", "session_id": sid, "selected": card, "unused": self.unused});
-
-        else if (!elem(self.unused, card))
+            self.send({"selected": card.value, "unused": unused});
+        else if (!elem(self.unused, card)){
             self.unused.push(card);
-
+            remove(self.hand, card);
+        }
     };
 
     self.unselect = function(card){
         remove(self.unused, card);
+        self.hand.push(card);
+    };
+
+    self.select = function(card){
+        if (!self.is_my_turn)
+            return;
+        else if (!elem(self.possible_cards, card.value)){
+            alert("You can't select");
+            return;
+        }
+        self.send({"mode": "select", "selected": card.value});
+    };
+
+    self.determine_adjutant = function(){
+        self.send({"mode": "adjutant", "adjutant": self.adjutant});
     };
 
     this.declare = function(){
-        self.send({"mode": "declare", "declaration": self.declaration, "session_id": sid});
+        self.send({"mode": "declare", "declaration": self.declaration});
     };
 
     this.start = function(){
@@ -72,11 +103,11 @@ app.controller("GameController", ["$scope", function($scope){
     };
 
     this.join = function(){
-        self.send({"mode": "join", "session_id": sid, user_id: self.user_id});
+        self.send({"mode": "join", user_id: self.user_id});
     };
 
     this.quit = function(){
-        self.send({"mode": "quit", "session_id": sid});
+        self.send({"mode": "quit"});
     };
 
     // utility
