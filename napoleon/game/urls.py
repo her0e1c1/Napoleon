d0274cef1e8618ecb5e33c3e4b25683816372538
her0e1c1 <her0e1c1@gmail.com>
2015-08-25 00:12:37 +0900

    add game/urls.py

---
+++ napoleon/game/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index, name="index"),
+    url(r'^create$', views.create, name="create"),
+    url(r'^detail/(?P<game_id>\d+)$', views.detail, name="detail"),
+    url(r'^json/state/(?P<room_id>\d+)$', views.game_state),
+]
