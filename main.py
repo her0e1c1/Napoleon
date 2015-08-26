05c2d5090bfdfadbce0dc73152fd1708052cdd47
her0e1c1 <her0e1c1@gmail.com>
2015-08-27 05:48:45 +0900

    remove unused handlers

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import tornado
 from tornado.options import options, define, parse_command_line
 from tornado.httpserver import HTTPServer
 from tornado.wsgi import WSGIContainer
-from tornado.websocket import WebSocketHandler
 from tornado.web import Application
 from tornado.web import FallbackHandler
 
@@ -18,23 +17,15 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "napoleon.settings")
 django.setup()
 
 
-class HelloHandler(WebSocketHandler):
-    def on_message(self, room_id):
-        self.write_message('Hello from tornado')
-
-settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "napoleon/static"),
-}
-
 if __name__ == "__main__":
     parse_command_line()
-    wsgi_app = WSGIContainer(django.core.handlers.wsgi.WSGIHandler())
+    from napoleon.wsgi import application
+    wsgi_app = WSGIContainer(application)
     app = Application([
         (r"/room/(?P<room_id>\d+)", GameHandler),
         (r".*", FallbackHandler, {"fallback": wsgi_app}),
-    ], **settings)
+    ])
     server = HTTPServer(app)
     port = int(os.environ.get("PORT", options.port))
     server.listen(port)
-    # tornado.ioloop.IOLoop.current().start()
     tornado.ioloop.IOLoop.instance().start()
