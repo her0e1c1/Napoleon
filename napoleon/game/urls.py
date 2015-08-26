cd5625d8fdade5c03694391b8d37f9ae6a2c4fb5
her0e1c1 <her0e1c1@gmail.com>
2015-08-26 17:43:39 +0900

    comment out for now

--- napoleon/game/urls.py
+++ napoleon/game/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index, name="index"),
     url(r'^create$', views.create, name="create"),
     url(r'^detail/(?P<game_id>\d+)$', views.detail, name="detail"),
-    url(r'^json/state/(?P<room_id>\d+)$', views.game_state),
+    # url(r'^json/state/(?P<room_id>\d+)$', views.game_state),
 ]
