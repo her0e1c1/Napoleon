2de78acc9c8cf7753b067bd38fa1717db50086a5
her0e1c1 <her0e1c1@gmail.com>
2015-08-27 07:16:45 +0900

    make url names simple

--- napoleon/game/urls.py
+++ napoleon/game/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index"),
-    url(r'^create$', views.create, name="create"),
-    url(r'^detail/(?P<game_id>\d+)$', views.detail, name="detail"),
-    url(r'^json/state/(?P<room_id>\d+)$', views.game_state),
+    url(r'^$', views.index),
+    url(r'^create$', views.create),
+    url(r'^room/(?P<game_id>\d+)$', views.detail),
+    url(r'^state/(?P<room_id>\d+)$', views.game_state),
 ]
