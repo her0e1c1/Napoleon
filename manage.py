d5c82e3c6881e8127fac838a858b83f31696e688
her0e1c1 <her0e1c1@gmail.com>
2015-08-27 08:46:36 +0900

    fix django_settings_module

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "napoleon.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "napoleon.settings.local")
 
     from django.core.management import execute_from_command_line
 
