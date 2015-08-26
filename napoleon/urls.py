b91b404a31ed98979c07c6195df44d4c06e1546d
her0e1c1 <her0e1c1@gmail.com>
2015-08-27 00:53:12 +0900

    rm an url prefix of games

--- napoleon/urls.py
+++ napoleon/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^games/', include('napoleon.game.urls')),
+    url(r'^', include('napoleon.game.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
