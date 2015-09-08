622afbf3da8227cd3405bcd90edae68f6be15544
her0e1c1 <her0e1c1@gmail.com>
2015-09-08 17:53:30 +0900

    test

--- napoleon/game/test.py
+++ napoleon/game/test.py
@@ -5,40 +5,53 @@ from napoleon.room.models import Room
 from napoleon.game import card
 
 
-# class StateTestCase(TestCase):
-#     fixtures = ["user.yaml", "room.yaml"]
+class DecideTestCase(TestCase):
 
-#     def setUp(self):
-#         self.room = Room.objects.get(pk=1)
-#         self.url_quit = reverse("napoleon.room.views.quit", kwargs={"room_id": self.room.id})
-#         self.url_join = reverse("napoleon.room.views.join", kwargs={"room_id": self.room.id})
+    def test_constant(self):
+        assert card.NUMBER_OF_CARDS == 54
+        assert card.NUMBER_OF_FACE_CARDS == 20
 
-#         self.c = Client(enforce_csrf_checks=False)
-#         assert self.c.login(username="test", password='test')
-#         self.user_id = int(self.c.session["_auth_user_id"])
-#         sid = self.c.session["_auth_user_hash"]
-#         self.pgs = state.PrivateGameState(user_id=self.user_id, session_id=sid, room_id=self.room.id)
+    def test_ace(self):
+        P = card.Plain
+        c, d, h, s = list(card.Suit)
+        assert card.decide([P(1, d), P(3, d), P(10, d)], c) == P(1, d)
+        assert card.decide([P(1, d), P(3, d), P(10, d)], d) == P(1, d)
+        assert card.decide([P(1, d), P(3, d), P(10, d)], h) == P(1, d)
+        assert card.decide([P(1, d), P(3, d), P(10, d)], s) == P(1, d)
 
-#     def tearDown(self):
-#         prefix = ("%s_" % self.room.id).encode("utf-8")
-#         for k in self.pgs.conn.keys("*"):
-#             if k.startswith(prefix):
-#                 self.pgs.conn.delete(k)
-#         self.c.logout
+    def test_trump(self):
+        P = card.Plain
+        c, d, h, s = list(card.Suit)
+        assert card.decide([P(1, c), P(3, c), P(10, c)], c) == P(1, c)
+        assert card.decide([P(1, d), P(3, c), P(10, d)], c) == P(3, c)
+        assert card.decide([P(1, d), P(3, h), P(10, s)], c) == P(10, s)  # lead won
+        assert card.decide([P(1, s), P(3, c), P(10, d)], c) == P(1, s)
 
-#     def test_join_and_quit(self):
-#         pgs = self.pgs
+    def test_joker(self):
+        P = card.Plain
+        JB = card.Joker.black
+        JR = card.Joker.red
+        c, d, h, s = list(card.Suit)
+        assert card.decide([P(1, c), JB, JR], c) == JB
+        assert card.decide([P(1, c), JB, JR], d) == JR
 
-#         response = self.c.post(self.url_join)
-#         assert response.status_code == 302
-#         assert pgs.player_ids == [self.user_id]
+    def test_same_two(self):
+        P = card.Plain
+        c, d, h, s = list(card.Suit)
+        assert card.decide([P(2, c), P(3, c), P(10, c)], c) == P(2, c)
 
-#         response = self.c.post(self.url_quit)
-#         assert response.status_code == 302
-#         assert pgs.player_ids == []
+        # no same two occurs
+        assert card.decide([P(2, c), P(3, c), c.right_jack], c) == c.right_jack
+        assert card.decide([P(2, c), P(3, c), P(10, c)], c, is_first_round=True) == P(10, c)
+
+    def test_yoromeki(self):
+        P = card.Plain
+        c, d, h, s = list(card.Suit)
+        assert card.decide([P(1, s), P(12, h), P(10, c)], c) == P(12, h)
+        assert card.decide([P(1, s), P(12, d), P(10, c)], c) == P(1, s)
 
 
-class State1TestCase(TestCase):
+class StateTestCase(TestCase):
     fixtures = ["user.yaml", "room.yaml"]
 
     def setUp(self):
@@ -47,21 +60,34 @@ class State1TestCase(TestCase):
             {"username": "test1", "password": "test"},
             {"username": "test2", "password": "test"},
         ]
-        self.players = []
+        self.player_clients = []
         for d in users:
             c = Client(enforce_csrf_checks=False)
             assert c.login(**d)
-            self.players.append(c)
+            self.player_clients.append(c)
 
         self.room = Room.objects.get(pk=1)
         self.state = state.GameState(self.room.id)
         self.url_join = reverse("napoleon.room.views.join", kwargs={"room_id": self.room.id})
+        self.url_quit = reverse("napoleon.room.views.quit", kwargs={"room_id": self.room.id})
 
     def tearDown(self):
         self.state.flush()
 
+    def test_join_and_quit(self):
+        c = self.player_clients[0]
+        user_id = int(c.session["_auth_user_id"])
+        # sid = c.session["_auth_user_hash"]
+        response = c.post(self.url_join)
+        assert response.status_code == 302
+        assert self.state.players == [state.Player(user_id, self.state)]
+
+        response = c.post(self.url_quit)
+        assert response.status_code == 302
+        assert self.state.players == []
+
     def _test_start(self):
-        for c in self.players:
+        for c in self.player_clients:
             assert c.post(self.url_join).status_code == 302
         self.state.start()
         hands = [p.hand for p in self.state.players]
@@ -94,6 +120,7 @@ class State1TestCase(TestCase):
         num = 6
         unused = p.hand[:num]
         hand = p.hand[num:] + self.state.rest
+        p.add_rest_to_hand()
         p.discard(unused)
         assert unused == self.state.unused
         assert sorted(hand) == sorted(p.hand)
