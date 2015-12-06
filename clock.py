97a11252ab624f47a3d946e7b6b966cd44d38123
her0e1c1 <her0e1c1@gmail.com>
2015-12-06 16:52:57 +0900

    call cron functions

---
+++ clock.py
@@ -0,0 +1,23 @@
+import os
+from django import setup
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "napoleon.settings.local")
+setup()
+
+
+@sched.scheduled_job('cron', hour=0)
+#@sched.scheduled_job('interval', seconds=1)
+def scheduled_finish_room():
+    print('finish room every day')
+    from napoleon.room.management.commands.finish_room import Command    
+    cmd = Command()
+    cmd.handle(**{"days": 1})
+
+
+@sched.scheduled_job('interval', seconds=1)
+def scheduled_finish_room1():
+    print("hoge")
+
+sched.start()
